4cb29d0840f2bd1e880bde2b22aef6a4662faf3f
Ifeanyi-design <ifeanyiagada123@gmail.com>
2026-01-03 00:01:44 +0100

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,10 +1,10 @@
 #(©)Codexbotz
-#@iryme
-
 from aiohttp import web
 from .route import routes
 
-async def web_server():
+async def web_server(client):
     web_app = web.Application(client_max_size=30000000)
+    # This is the Secret Sauce: We attach the client to the app
+    web_app['client'] = client 
     web_app.add_routes(routes)
     return web_app
